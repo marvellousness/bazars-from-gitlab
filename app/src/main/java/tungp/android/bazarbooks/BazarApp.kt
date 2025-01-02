c9abcddf6cf8cf19101ae8f8129d65d7c084e2f8
Tung Phan <tungphan@asnet.com.vn>
2025-01-02 15:20:00 +0700

    Add Hilt

---
+++ app/src/main/java/tungp/android/bazarbooks/BazarApp.kt
@@ -0,0 +1,7 @@
+package tungp.android.bazarbooks
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BazarApp : Application()
--- app/src/main/java/tungp/android/bazarbooks/MainActivity.kt
+++ app/src/main/java/tungp/android/bazarbooks/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import tungp.android.bazarbooks.ui.theme.BazarBooksTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
