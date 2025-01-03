c5c1b96f8c5fb7a34d54d18071d53fd1584bef8e
Tung Phan <tungphan@asnet.com.vn>
2025-01-03 10:21:26 +0700

    Use theme in Onboarding screen

--- app/src/main/java/tungp/android/bazarbooks/screens/onboarding/OnboardingScreen.kt
+++ app/src/main/java/tungp/android/bazarbooks/screens/onboarding/OnboardingScreen.kt
@@ -3,31 +3,45 @@ package tungp.android.bazarbooks.screens.onboarding
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import tungp.android.bazarbooks.components.BazarSurface
+import tungp.android.bazarbooks.components.button.PrimaryButton
 import tungp.android.bazarbooks.navigation.AuthRouteScreen
 import tungp.android.bazarbooks.navigation.Graph
+import tungp.android.bazarbooks.ui.theme.BazarTheme
 
 @Composable
 fun OnboardingScreen(navController: NavController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Onboarding Screen")
-        Button(onClick = {
-            navController.navigate(Graph.AuthGraph) {
-                popUpTo(AuthRouteScreen.Login.route) {
-                    inclusive = true
-                }
-            }
-        }) {
-            Text(text = "Go to login")
+    BazarSurface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Onboarding Screen")
+            PrimaryButton(
+                text = "Go to login",
+                onClick = {
+                    navController.navigate(Graph.AuthGraph) {
+                        popUpTo(AuthRouteScreen.Login.route) {
+                            inclusive = true
+                        }
+                    }
+                })
         }
+    }
+}
+
+@Preview
+@Composable
+private fun OnboardingScreenPreview() {
+    BazarTheme {
+        OnboardingScreen(rememberNavController())
     }
 }
